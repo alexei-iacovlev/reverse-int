d9d094e29f193df1c256299e52a8b652d660ff44
alexei-iacovlev <99271957+alexei-iacovlev@users.noreply.github.com>
2023-01-04 10:26:33 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let str = String(Math.abs(n));
+	let strReverse = str.split("").reverse().join("");
+	return Number(strReverse);
 }
